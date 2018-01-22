36fdd438e32909b228cd0c8e08552ed5b3a5044f
fengf <fengf@hlongsky.com>
2018-01-22 21:17:05 +0800

    wx-pro add imgs

--- pro/pages/logs/logs.js
+++ pro/pages/logs/logs.js
@@ -1,15 +1,52 @@
 //logs.js
 const util = require('../../utils/util.js')
-
+const sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
   data: {
-    logs: []
+    logs: [],
+    grids: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    tabs: ["选项一", "选项二", "选项三"],
+
+    sliderOffset: 0,
+    // tabbar
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换
+    currentTab: 0,
   },
   onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
       })
     })
+  },
+  bindChange: function (e) {
+    var that = this;
+    console.log(e);
+    that.setData({
+      currentTab: e.detail.current,
+      sliderOffset: e.detail.current * that.data.winWidth/3,
+    });
+  },
+  tabClick: function (e) {
+    console.log(e)
+    if (this.data.currentTab == e.currentTarget.id) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: parseInt(e.currentTarget.id),
+        sliderOffset: e.currentTarget.offsetLeft,
+      })
+    }
   }
 })
--- pro/pages/my/my.js
+++ pro/pages/my/my.js
@@ -1,14 +1,17 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
   },
+
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
@@ -16,39 +19,48 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        console.log(res);
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success: () => {
+              wx.getUserInfo({
+                success: res => {
+                  console.log(res);
+                  this.setData({
+                    userInfo: res.userInfo,
+                  })
+                }
+              })
+            }
+          })
+        } else {
+          wx.getUserInfo({
+            success: res => {
+              console.log(`已经授权${res}`);
+              this.setData({
+                userInfo: res.userInfo,
+              })
+            }
           })
         }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      },
+      fail: (fail) => {
+        console.log(fail)
+      },
+      complete: (msg) => {
+        console.log(msg)
+      }
     })
-  }
+  },
+
 })
